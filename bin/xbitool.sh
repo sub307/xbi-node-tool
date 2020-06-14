0a7b33534d9654d3ba5e9be4f7d495acc303acb0
sub307 <39446798+sub307@users.noreply.github.com>
2020-06-14 19:58:49 +0200

    Changes names for XBI

--- bin/xbitool.sh
+++ bin/xbitool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# axerunner - main executable
+# xbitool - main executable - based on axerunner
 # installs, updates, and manages axe daemons and wallets
 
 # Copyright (c) 2015-2019 moocowmoo - moocowmoo@masternode.me
@@ -9,20 +9,21 @@
 # check we're running bash 4 -------------------------------------------------
 
 RED='\033[0;31m'
+BLUE="\033[0;34m"
 NC='\033[0m'
 echo -e "${RED}
 
-  █████╗    ██╗  ██╗   ███████╗
- ██╔══██╗   ╚██╗██╔╝   ██╔════╝
- ███████║    ╚███╔╝    █████╗
- ██╔══██║    ██╔██╗    ██╔══╝
- ██║  ██║   ██╔╝ ██╗   ███████╗
- ╚═╝  ╚═╝   ╚═╝  ╚═╝   ╚══════╝
+██╗  ██╗   ██████╗   ██╗
+╚██╗██╔╝   ██╔═══█╗  ██║
+ ╚███╔╝    ██████╔╝  ██║
+ ██╔██╗    ██╔═══█╗  ██║
+██╔╝ ██╗   ██████╔╝  ██║
+╚═╝  ╚═╝   ╚═════╝   ╚═╝
 
 ${NC}"
 
-if [[ ${BASH_VERSION%%.*} != '4' ]];then
-    die "axerunner requires bash version 4. please update. exiting."
+if [[ ${BASH_VERSION%%.*} != '1' ]];then
+    die "xbitool requires bash version 1. please update. exiting."
 fi
 
 # parse any command line switches --------------------------------------------
@@ -50,25 +51,25 @@ shift $(($OPTIND - 1))
 
 # load common functions ------------------------------------------------------
 
-AXERUNNER_BIN=$(readlink -f $0)
-AXERUNNER_GITDIR=$(readlink -f ${AXERUNNER_BIN%%/bin/${AXERUNNER_BIN##*/}})
-source $AXERUNNER_GITDIR/lib/axerunner_functions.sh
+XBITOOL_BIN=$(readlink -f $0)
+XBITOOL_GITDIR=$(readlink -f ${XBITOOL_BIN%%/bin/${XBITOOL_BIN##*/}})
+source $XBITOOL_GITDIR/lib/axerunner_functions.sh
 
 # load language packs --------------------------------------------------------
 
 declare -A messages
 
 # set all default strings
-source $AXERUNNER_GITDIR/lang/en_US.sh
+source $XBITOOL_GITDIR/lang/en_US.sh
 
 # override if configured
 lang_type=${LANG%%\.*}
-[[ -e $AXERUNNER_GITDIR/lang/$lang_type.sh ]] && source $AXERUNNER_GITDIR/lang/$lang_type.sh
+[[ -e $XBITOOL_GITDIR/lang/$lang_type.sh ]] && source $XBITOOL_GITDIR/lang/$lang_type.sh
 
 # process switch overrides ---------------------------------------------------
 
 # show version and exit if requested
-[[ $VERSION || $1 == 'version' ]] && echo $AXERUNNER_VERSION && exit 0
+[[ $VERSION || $1 == 'version' ]] && echo $XBITOOL_VERSION && exit 0
 
 # show help and exit if requested or no command supplied - TODO make command specific
 [[ $HELP || -z $1 ]] && usage && exit 0
@@ -78,35 +79,35 @@ _check_dependencies $@
 
 # have command, will travel... -----------------------------------------------
 
-echo -e "${C_CYAN}${messages["axerunner_version"]} $AXERUNNER_VERSION$AXERUNNER_CHECKOUT${C_NORM} - ${C_GREEN}$(date)${C_NORM}"
+echo -e "${C_CYAN}${messages["xbitool_version"]} $XBITOOL_VERSION$XBITOOL_CHECKOUT${C_NORM} - ${C_GREEN}$(date)${C_NORM}"
 
 # do awesome stuff -----------------------------------------------------------
 COMMAND=''
 case "$1" in
         restart)
             COMMAND=$1
-            _find_axe_directory
-            _check_axed_state
+            _find_xbi_directory
+            _check_xbid_state
             # TODO, show uptime: ps --no-header -o pid,etime $(cat $INSTALL_DIR/axe.pid) | awk '{print $2}'
             case "$2" in
                 now)
-                    restart_axed
+                    restart_xbid
                     ;;
                 *)
                     echo
-                    pending "restart axed? "
+                    pending "restart xbid? "
                     confirm "[${C_GREEN}y${C_NORM}/${C_RED}N${C_NORM}] $C_CYAN" && \
-                        restart_axed
+                        restart_xbid
                     ;;
             esac
             ;;
         update)
             COMMAND=$1
             pending "${messages["gathering_info"]}"
-            _check_axerunner_updates
-            _find_axe_directory
+            _check_xbitool_updates
+            _find_xbi_directory
             _get_versions
-            _check_axed_state
+            _check_xbid_state
             ok " ${messages["done"]}"
             if [ ! -z "$2" ]; then
                 if [ "$2" == '-y' ] || [ "$2" == '-Y' ]; then
@@ -117,12 +118,12 @@ case "$1" in
             if [ ! -z "$ARM" ] && [ $BIGARM -eq 0 ]; then
                 die "$COMMAND not supported yet on this platform."
             fi
-            update_axed
+            update_xbid
             ;;
         install)
             COMMAND=$1
             pending "${messages["gathering_info"]}"
-            _check_axerunner_updates
+            _check_xbitool_updates
             _get_versions
             ok " ${messages["done"]}"
             if [ ! -z "$ARM" ] && [ $BIGARM -eq 0 ]; then
@@ -131,11 +132,10 @@ case "$1" in
             if [ ! -z "$2" ]; then
                 APP=$2;
                 if [ "$APP" == 'sentinel' ]; then
-                    _find_axe_directory
-                    install_sentinel
+                    _find_xbi_directory
                 elif [ "$APP" == 'unattended' ]; then
                     UNATTENDED=1
-                    install_axed
+                    install_xbid
                 else
                     echo "don't know how to install: $2"
                 fi
@@ -144,7 +144,7 @@ case "$1" in
                 # axerunner
                 # ???
             else
-                install_axed
+                install_xbid
                 show_message_configure
             fi
             quit
@@ -152,57 +152,57 @@ case "$1" in
         reinstall)
             COMMAND=$1
             pending "${messages["gathering_info"]}"
-            _check_axerunner_updates
-            _find_axe_directory
+            _check_xbitool_updates
+            _find_xbi_directory
             _get_versions
-            _check_axed_state
+            _check_xbid_state
             REINSTALL=1
             ok " ${messages["done"]}"
             if [ ! -z "$ARM" ] && [ $BIGARM -eq 0 ]; then
                 die "$COMMAND not supported yet on this platform."
             fi
-            update_axed
+            update_xbid
             ;;
         sync)
             COMMAND=$1
-            cd $AXERUNNER_GITDIR
+            cd $XBITOOL_GITDIR
             git fetch --prune origin +refs/tags/*:refs/tags/*
             git remote update -p
             if [ -z $(git config user.email) ] ; then
-                git config user.email "axerunneruser"
-                git config user.name "axerunneruser"
+                git config user.email "xbitooluser"
+                git config user.name "xbitooluser"
             fi
             git stash
             git checkout master
             git reset --hard origin/master
 
-            if [ -e $AXERUNNER_GITDIR/PREVIOUS_VERSION ]; then
+            if [ -e $XBITOOL_GITDIR/PREVIOUS_VERSION ]; then
                 echo '--------------'
-                cat_until "^$( cat $AXERUNNER_GITDIR/PREVIOUS_VERSION ) " $AXERUNNER_GITDIR/CHANGELOG.md | sed \
+                cat_until "^$( cat $XBITOOL_GITDIR/PREVIOUS_VERSION ) " $XBITOOL_GITDIR/CHANGELOG.md | sed \
                     -e "/^0\./s/^/$(echo -e $C_YELLOW)/"    -e "/^0\./s/$/$(echo -e $C_NORM)/" \
                     -e "/enh - /s/^/$(echo -e $C_GREEN)/"   -e "/enh - /s/$/$(echo -e $C_NORM)/" \
                     -e "/compat - /s/^/$(echo -e $C_YELLOW)/" -e "/compat - /s/$/$(echo -e $C_YELLOW)/" \
                     -e "/config - /s/^/$(echo -e $C_CYAN)/" -e "/config - /s/$/$(echo -e $C_NORM)/" \
                     -e "/bugfix - /s/^/$(echo -e $C_RED)/"  -e "/bugfix - /s/$/$(echo -e $C_NORM)/"
                 echo '--------------'
-                rm $AXERUNNER_GITDIR/PREVIOUS_VERSION
+                rm $XBITOOL_GITDIR/PREVIOUS_VERSION
             fi
 
             if [ ! -z "$2" ]; then
                 self=${0##*/};
                 shift;
-                exec $AXERUNNER_GITDIR/$self $@
+                exec $XBITOOL_GITDIR/$self $@
             fi
             quit "${messages["quit_uptodate"]}"
             ;;
         branch)
             COMMAND=$1
-            cd $AXERUNNER_GITDIR
+            cd $XBITOOL_GITDIR
             git fetch --prune origin +refs/tags/*:refs/tags/*
             git remote update -p
             if [ -z $(git config user.email) ] ; then
-                git config user.email "axerunneruser"
-                git config user.name "axerunneruser"
+                git config user.email "xbitooluser"
+                git config user.name "xbitooluser"
             fi
             BRANCH_OK=$(git for-each-ref --format='%(refname)' refs/remotes/origin | sed -e 's|refs/remotes/origin/||g' | grep "^${2}\$" | wc -l)
             if [ $BRANCH_OK -gt 0 ];then
@@ -217,24 +217,24 @@ case "$1" in
         vote)
             COMMAND=$1
             pending "${messages["gathering_info"]}"
-            _check_axerunner_updates
-            _find_axe_directory
+            _check_xbitool_updates
+            _find_xbi_directory
             _get_versions
-            _check_axed_state
+            _check_xbid_state
             ok " ${messages["done"]}"
             echo
             export AXE_CLI AXERUNNER_PID=$$
-            /usr/bin/env python $AXERUNNER_GITDIR/bin/axevote.py
+            /usr/bin/env python $XBITOOL_GITDIR/bin/axevote.py
             quit 'Exiting.'
             ;;
         status)
             COMMAND=$1
             pending "${messages["gathering_info"]}"
-            _check_axerunner_updates
-            _find_axe_directory
+            _check_xbitool_updates
+            _find_xbi_directory
             _get_versions
-            _check_axed_state
-            get_axed_status
+            _check_xbid_state
+            get_xbid_status
             get_host_status
             ok " ${messages["done"]}"
             echo
